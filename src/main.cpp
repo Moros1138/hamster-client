7bed2031583242c446bd90fcd8907ce2fc7d304e
Moros Smith <moros1138@gmail.com>
2024-08-23 22:56:27 -0400

    remove proof of concept code

--- src/main.cpp
+++ src/main.cpp
@@ -1,130 +1,5 @@
 #include "olcPixelGameEngine.h"
 
-#include <chrono>
-#include <emscripten.h>
-
-EM_JS(int, InitSession, (), {
-    return Asyncify.handleSleep(function(wakeUp) {
-        fetch('/session', { method: 'POST', credentials: 'same-origin' })
-            .then((response) =>
-            {
-                return response.ok
-                ? response.json().then((data) => JSON.stringify(data, null, 2))
-                : Promise.reject(new Error("Unexpected response"));
-            })
-            .then((message) =>
-            {
-                console.log(message);
-                wakeUp(1);
-            })
-            .catch((err) =>
-            {
-                console.error(err.message);
-                wakeUp(0);
-            })    
-    });
-});
-
-EM_JS(int, SetRacerName, (const char* str), {
-    
-    let racerName = UTF8ToString(str);
-    
-    return Asyncify.handleSleep(function(wakeUp) {
-        fetch('/name', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ userName: racerName })
-        }).then((response) =>
-        {
-            return response.ok
-            ? response.json().then((data) => JSON.stringify(data, null, 2))
-            : Promise.reject(new Error("Unexpected response"));
-        })
-        .then((message) =>
-        {
-            wakeUp(1);
-        })
-        .catch((err) =>
-        {
-            console.error(err.message);
-            wakeUp(0);
-        })    
-    });
-});
-
-EM_JS(int, StartRace, (), {
-    
-    return Asyncify.handleSleep(function(wakeUp) {
-        fetch('/race', {
-            method: 'POST',
-            credentials: 'same-origin',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({})
-        }).then((response) =>
-        {
-            return response.ok
-            ? response.json()
-            : Promise.reject(new Error("Unexpected response"));
-        })
-        .then((message) =>
-        {
-            Module.hamsterRaceId = message.raceId;
-            wakeUp(1);
-        })
-        .catch((err) =>
-        {
-            console.error(err.message);
-            wakeUp(0);
-        })    
-    });
-});
-
-EM_JS(int, FinishRace, (const char* raceMap, const char* raceColor, int raceTime), {
-    
-    if(Module.hamsterRaceId === undefined)
-    {
-        console.error("Trying to finish a race that never start!");
-        return 0;
-    }
-    
-    const raceData = {
-        raceId: Module.hamsterRaceId,
-        raceTime: raceTime,
-        raceMap: UTF8ToString(raceMap),
-        raceColor: UTF8ToString(raceColor),
-    };
-    
-    return Asyncify.handleSleep(function(wakeUp) {
-        fetch('/race', {
-            method: 'PATCH',
-            credentials: 'same-origin',
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify(raceData)
-        }).then((response) =>
-        {
-            return response.ok
-            ? response.json().then((data) => JSON.stringify(data, null, 2))
-            : Promise.reject(new Error("Unexpected response"));
-        })
-        .then((message) =>
-        {
-            wakeUp(1);
-        })
-        .catch((err) =>
-        {
-            console.error(err.message);
-            wakeUp(0);
-        })    
-    });
-});
-
 class Test : public olc::PixelGameEngine
 {
 public:
@@ -153,7 +28,6 @@ public:
     };
 
     State state = State::Init;
-    std::chrono::time_point<std::chrono::system_clock> tp1, tp2;
     
     bool OnUserUpdate(float fElapsedTime) override
     {
@@ -167,29 +41,16 @@ public:
             switch(state)
             {
                 case State::Init:
-                    InitSession();
-                    state = State::RacerName;
                     break;
                 case State::RacerName:
-                    SetRacerName("Jonny");
                     state = State::BeginRace;
                     break;
                 case State::BeginRace:
-                    StartRace();
-                    tp1 = std::chrono::system_clock::now();
-            		tp2 = std::chrono::system_clock::now();
-                    state = State::EndRace;
                     break;
                 case State::EndRace:
-                    tp2 = std::chrono::system_clock::now();
-                    std::chrono::duration<double, std::milli> duration = tp2 - tp1;
-                    int raceTime = static_cast<int>(duration.count());
-                    std::cout << "RACETIME: " << raceTime << "\n";
-                    FinishRace("map-123", "red", raceTime);
                     break;
             }
         }
-        auto start = std::chrono::high_resolution_clock::now();
         return true;
     }
 };
